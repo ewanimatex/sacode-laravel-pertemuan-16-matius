7e79a342c13b20ab895114b8bffdd6425f6bf9ab
Matius ewani <ewanimatex@gmail.com>
2024-09-30 04:11:21 -0700

    menambahkan file index.php

---
+++ detail.php
@@ -0,0 +1,23 @@
+<?php
+$id = $_GET['id'],
+
+
+foreach($mahasiswa as $m)
+
+{
+if($m ['id']==$id)
+
+echo $m['nama']. "<br>",
+
+echo $m['nim']. "<br>",
+
+echo $m['alamat']. "<br>",
+
+echo $m['Jurusan']. "<br>",
+
+
+
+}
+
+
+?>
